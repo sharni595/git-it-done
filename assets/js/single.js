e0a641e629f47955ef8ee6a749b04565ae642f28
Sharni Zaugg <sharni.greener@gmail.com>
2021-04-14 18:42:11 -0600

    add error handling and document.location

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+    if (repoName){
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName;
+    } else{
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function(repo){
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,7 +29,7 @@ var getRepoIssues = function(repo){
 
                 })
             }else{
-                alert("There was a problem with your request!");
+                document.location.replace("./index.html");
             }
         });
 };
@@ -68,4 +81,5 @@ var displayWarning = function(repo){
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+
+getRepoName();
